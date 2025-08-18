b6f9159d8b7dbee78d50dc9118bb9621fe0c3a32
[Anuragh] <kannanuragh@gmail.com>
2025-08-18 10:21:36 +0530

    Add file-to-bits and bits-to-file conversion

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from PIL import Image
 import os, sys, subprocess, binascii
 from shutil import rmtree
 
-def main():
-    print("Project initialized.")
+def bits_2_file(bits, fname):
+    with open(fname, 'wb') as f:
+        for i in range(0, len(bits), 8):
+            byte = ''.join(bits[i:i+8])
+            f.write(int(byte, 2).to_bytes(1, 'big'))
+    print(f"Wrote {len(bits)} bits to {fname}")
 
-if __name__ == '__main__':
-    main()
+def file_2_bits(fname):
+    bits = []
+    with open(fname, "rb") as f:
+        byte = f.read(1)
+        while byte:
+            cur_bits = bin(byte[0])[2:].zfill(8)
+            bits.extend(list(cur_bits))
+            byte = f.read(1)
+    return bits
+
+def main():
+    bits = file_2_bits("data/test.zip")
+    bits_2_file(bits, "test-copy.zip")
